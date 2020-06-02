c53cb0c232eb44c1b3fc835e8703c6ec55fe9d20
Anthony <38259824+arn-foto@users.noreply.github.com>
2020-06-02 14:25:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import TodoList from './components/TodoList';
 
 class App extends Component {
-	state = {
-		count: 0
-	};
-
-	increment = () => {
-		this.setState({
-			count: this.state.count + 1
-		});
-	};
-
-	decrement = () => {
-		this.setState({
-			count: this.state.count - 1
-		});
-	};
 
 	render() {
 		return (
